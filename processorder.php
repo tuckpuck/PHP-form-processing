56c374623e66361d77e1d1ecff13d323834b3391
tuckpuck <tucker.triggs@yahoo.com>
2019-01-05 15:52:44 -0700

    added dynamic item counts to order receipt

--- processorder.php
+++ processorder.php
@@ -1,3 +1,12 @@
+<?php // create variable names
+    $tireqty = $_POST['tireqty'];
+    $oilqty = $_POST['oilqty'];
+    $sparkqty = $_POST['sparkqty'];
+    $tireqty=htmlspecialchars($tireqty);
+    $oilqty=htmlspecialchars($oilqty);
+    $sparkqty=htmlspecialchars($sparkqty);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +15,14 @@
 <body>
     <h1>Bob's Auto Parts</h1>
     <h2>Order Results</h2>
-    <?php echo '<p>Order processed.</p>';
+    <?php 
+    echo "tires: $tireqty <br/>";
+    echo "oil: $oilqty <br/>";
+    echo "spark: $sparkqty <br/>";
+
+    echo '<p>Order processed at ';
+    echo date('H:i, jS F Y');
+    echo '</p>';
     ?> 
 </body>
 </html>
